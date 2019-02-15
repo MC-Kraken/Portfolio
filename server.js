4872ade84164a1bb9f3533c58549e464b1d9daca
Student <student@Admins-iMac.local>
2019-02-15 17:06:28 -0600

    finished nav bar styles

--- server.js
+++ server.js
@@ -3,9 +3,22 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const mysql = require('mysql');
 const port = '3000';
-const contactInfo = require('./db/db.js')
-const insertInfo = require('./db/db.js')
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+//////////CONNECTION//////////
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'BeGre@t2019',
+    database: 'portfolio_contact'
+});
 
 
 //////////SETTING VIEW ENGINE TO EJS//////////
@@ -35,6 +48,10 @@ app.get('/projects', function (req, res) {
 
 app.get('/contact', function (req, res) {
     res.render(pathJoin + '/contact');
+    
+    let cActive = document.getElementById('contact')
+    
+    cActive.classList.add('active')
 });
 
 //////////CUSTOM 404 PAGE//////////
@@ -46,40 +63,22 @@ app.get('*', function (req, res) {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //////////POST RESPONSE FOR CONTACT PAGE//////////
-
-////////MYSQL POST FUNCTION//////////
 app.post('/contact', (req, res) => {
-    
-    // insertInfo.insertInfo()
-    //     .then(results => res.send(results))
-    //     .catch(e => console.log(e));
-    //     console.log(`${name}`)
 
     ////////POST RESPONSE//////////
     res.send('Thanks for the email ' + req.body.name + '. I will get back to you in a timely manner.');
-    //////////STORE NAME IN JSON FILE//////////
-    fs.appendFileSync('userData.json', JSON.stringify(req.body.name), err => {
 
-        if (err)
-            console.log("You've made an error")
-    })
+    //////////SEND TO DATABASE//////////
+    let query = `INSERT INTO contact_info(name, email, message) VALUES(?, ?, ?)`;
 
+    let data = [`${req.body.name}`, `${req.body.email}`, `${req.body.message}`]
 
-    
-
-
-    // //////////STORE EMAIL IN JSON FILE//////////
-    // fs.appendFileSync('userData.json', JSON.stringify(req.body.email), err => {
-
-    //     if (err)
-    //         console.log("You've made an error")
-    // })
-
-    // fs.appendFileSync('userData.json', JSON.stringify(req.body.message), err => {
-
-    //     if (err)
-    //         console.log("You've made an error")
-    // })
+    pool.query(query, data, (err, results, fields) => {
+        if (!err)
+            res.send(results);
+        else
+            console.log(err);
+    });
 });
 
 
