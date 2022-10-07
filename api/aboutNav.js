eb02bda3d1aebfea00de90543c138d5531485c3e
blakemccracken <blake.mccracken@integrate.com>
2022-10-07 13:11:33 -0700

    adding vercel.json

--- api/aboutNav.js
+++
@@ -1,6 +0,0 @@
-import path from "path";
-
-export default function aboutNav(request, response) {
-    const pathJoin = path.join(__dirname + '/views');
-    return response.render(pathJoin + "/about");
-}
