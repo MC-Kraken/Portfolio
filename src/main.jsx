119a64cf932de3d54664ad3391390eb25253284b
blakemccracken <blake.mccracken@integrate.com>
2023-10-16 08:43:19 -0700

    getting rid of portrait mode only, doesn't work

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppRouter from "./components/AppRouter.jsx";
-import { PortraitModeOnly } from "./components/PortraitModeOnly.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <PortraitModeOnly>
-            <AppRouter />
-        </PortraitModeOnly>
+        <AppRouter />
     </React.StrictMode>,
 )
