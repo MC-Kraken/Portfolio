fcd78ef3779f51ab5f52abdeddc153ddac26fca1
blakemccracken <blake.mccracken@integrate.com>
2024-01-01 21:37:29 -0800

    seem to have strava auth working for good now

--- api/getStravaTokenWithRefreshToken.js
+++ api/getStravaTokenWithRefreshToken.js
@@ -1,9 +1,10 @@
-import AWS from "aws-sdk";
-import fetch from "node-fetch";
+import AWS from 'aws-sdk';
+import fetch from 'node-fetch';
 import allowCors from "./_allowCors.js";
 
 async function handler(request, response) {
-    const tokenExchangeUrl = "https://www.strava.com/api/v3/oauth/token";
+    const tokenExchangeUrl = `https://www.strava.com/api/v3/oauth/token`;
+    let refreshToken = null;
 
     AWS.config.update({
         // eslint-disable-next-line no-undef
@@ -15,15 +16,29 @@ async function handler(request, response) {
     const secretsManager = new AWS.SecretsManager();
     const secretName = "STRAVA_REFRESH_TOKEN";
 
+    // Retrieve the secret value
+    secretsManager.getSecretValue({SecretId: secretName}, (err, data) => {
+        if (err) {
+            console.error('Error retrieving secret:', err);
+        } else {
+            // Parse and use the secret value
+            const secret = JSON.parse(data.SecretString);
+            refreshToken = secret.STRAVA_REFRESH_TOKEN;
+        }
+    });
+
     fetch(tokenExchangeUrl, {
         method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
         body: new URLSearchParams({
             // eslint-disable-next-line no-undef
             client_id: process.env.VITE_STRAVA_CLIENT_ID,
             // eslint-disable-next-line no-undef
             client_secret: process.env.VITE_STRAVA_CLIENT_SECRET,
-            grant_type: "authorization_code",
-            code: request.body.code
+            grant_type: 'refresh_token',
+            refresh_token: refreshToken
         })
     })
         .then(res => res.json())
@@ -33,16 +48,17 @@ async function handler(request, response) {
                 SecretString: JSON.stringify({refreshToken: data.refresh_token}),
             }, (err, data) => {
                 if (err) {
-                    console.error("Error updating secret:", err);
+                    console.error('Error updating secret:', err);
                 } else {
-                    console.log("Secret updated successfully:", data);
+                    console.log('Secret updated successfully:', data);
                 }
             });
-
-            return response.status(200).json({accessToken: data.access_token})
+            return response.status(200).json({
+                accessToken: data.access_token
+            })
         })
         .catch(error => {
-            console.error("Error:", error);
+            console.error('Error:', error);
         });
 }
 
--- src/screens/Activity.jsx
+++ src/screens/Activity.jsx
@@ -2,8 +2,91 @@ import { Col, Container, Row } from "react-bootstrap";
 import { constants } from "../constants.js";
 import { Navigation } from "../components/Navigation.jsx";
 import Footer from "../components/Footer.jsx";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Activity() {
+    const [athlete, setAthlete] = useState();
+    const getAthleteUrl = "https://www.strava.com/api/v3/athlete";
+    const getActivitiesUrl = "https://www.strava.com/api/v3/athlete/activities";
+    const [activities, setActivities] = useState();
+    const location = useLocation();
+    const accessToken = localStorage.getItem("strava_access_token");
+    const queryParams = new URLSearchParams(location.search);
+    const authCode = queryParams.get('code');
+
+    useEffect(() => {
+        if ((!accessToken || accessToken === 'undefined') && !authCode) {
+            // testing with activity:read_all scope. Remove the _all when done testing private activities
+            // update redirect_uri with prod domain when deployed
+            window.location.href = `https://www.strava.com/oauth/authorize?client_id=${import.meta.env.VITE_STRAVA_CLIENT_ID}&redirect_uri=http://localhost:5173/activity&response_type=code&scope=read,activity:read_all`
+        }
+
+        if (authCode) {
+            fetch('http://localhost:3000/api/getStravaTokenWithAuthCode', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    code: authCode
+                })
+            })
+                .then(res => res.json())
+                .then(data => {
+                    localStorage.setItem("strava_access_token", data.accessToken);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        }
+    }, [authCode, accessToken]);
+
+    if (accessToken?.expires === 0) {
+        // get new token with refresh token, use serverless endpoint
+        fetch('http://localhost:3000/api/getStravaTokenWithRefreshToken', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            //do I have to include a body? Delete this if not
+        })
+            .then(res => res.json())
+            .then(data => {
+                localStorage.setItem("strava_access_token", data.access_token);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+
+
+    useEffect(() => {
+        if (accessToken) {
+            fetch(getAthleteUrl, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${accessToken}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => setAthlete(data))
+        }
+    }, [accessToken])
+
+    useEffect(() => {
+        if (accessToken) {
+            fetch(getActivitiesUrl, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("strava_access_token")}`
+                }
+            })
+                .then(res => res.json())
+                .then(data => setActivities(data))
+        }
+    }, [accessToken])
+
     return (
         <>
             <Navigation current={constants.screens.activity} />
@@ -25,6 +108,20 @@ export default function Activity() {
                         Check back soon!
                     </Col>
                 </Row>
+                {athlete &&
+                    <Row className="text-center">
+                        <Col>
+                            <h1>{`${athlete.firstname} ${athlete.lastname}`}</h1>
+                        </Col>
+                    </Row>
+                }
+                {activities &&
+                    <Row className="text-center">
+                        <Col>
+                            <h1>{`${activities[0].name}`}</h1>
+                        </Col>
+                    </Row>
+                }
             </Container>
             <Footer />
         </>
