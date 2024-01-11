fa1ff54266d66bf08892f26302c7685c3d35b5a5
blakemccracken <blake.mccracken@integrate.com>
2024-01-11 08:52:56 -0800

    undoing photo frame for now, broken on mobile

--- src/screens/Resume.jsx
+++ src/screens/Resume.jsx
@@ -15,27 +15,27 @@ export const Resume = () => {
                 </Row>
             </Container>
             <Container>
-                {/*<Row className="resume">*/}
-                {/*    <Col className="thumbnail"></Col>*/}
-                {/*</Row>*/}
-                <div className="bg__layout-wrapper">
-                    <div className="bg__frame">
-                        <div className="bg__picture-wrapper">
-                            <div className="bg__frame-inner-shadow-outer">
-                                <div className="bg__frame-inner-shadow"></div>
-                            </div>
-                            <div className="bg__frame-inner-shine-outer">
-                                <div className="bg__frame-inner-shine"></div>
-                            </div>
-                            <div className="bg__frame-inner-photo-shadow-outer">
-                                <div className="bg__frame-inner-photo-shadow"></div>
-                            </div>
-                            <img className="bg__picture"
-                                 src="/resume.png"
-                                 alt="" />
-                        </div>
-                    </div>
-                </div>
+                <Row className="resume">
+                    <Col className="thumbnail"></Col>
+                </Row>
+                {/*<div className="bg__layout-wrapper">*/}
+                {/*    <div className="bg__frame">*/}
+                {/*        <div className="bg__picture-wrapper">*/}
+                {/*            <div className="bg__frame-inner-shadow-outer">*/}
+                {/*                <div className="bg__frame-inner-shadow"></div>*/}
+                {/*            </div>*/}
+                {/*            <div className="bg__frame-inner-shine-outer">*/}
+                {/*                <div className="bg__frame-inner-shine"></div>*/}
+                {/*            </div>*/}
+                {/*            <div className="bg__frame-inner-photo-shadow-outer">*/}
+                {/*                <div className="bg__frame-inner-photo-shadow"></div>*/}
+                {/*            </div>*/}
+                {/*            <img className="bg__picture"*/}
+                {/*                 src="/resume.png"*/}
+                {/*                 alt="" />*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
                 <Row>
                     <Col className="text-center d-flex justify-content-end">
                         <a href="/BlakeMcCrackenResume.pdf" download rel="noopener noreferrer">
