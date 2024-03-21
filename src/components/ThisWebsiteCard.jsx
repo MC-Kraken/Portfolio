c0c535bcb51ba39b4bbc835778ddf9f951b18b42
blakemccracken <blake.mccracken@integrate.com>
2024-03-20 18:20:26 -0700

    using new image for ThisWebsiteCard.jsx

--- src/components/ThisWebsiteCard.jsx
+++ src/components/ThisWebsiteCard.jsx
@@ -4,11 +4,14 @@ export default function ThisWebsiteCard() {
     return (
         <div className={"card-container"}>
             <Card style={{width: '18rem'}}>
-                <Card.Img className={"bg-dark text-white"} variant="top" src="/favicon-resized.png" />
+                <Card.Img className={"bg-dark text-white"} variant="top" src="/this-website.png" />
                 <Card.Body className={"bg-dark text-white"}>
                     <Card.Title>This Website!</Card.Title>
                     <Card.Text>
                         Technologies used: Vite, React, SCSS, Bootstrap, Strava API, AWS Secrets Manager, Vercel Functions, Leaflet, OpenStreetMap, Google Apps Script
+                        <br/>
+                        <br/>
+                        New features are being added regularly, so stay tuned.
                     </Card.Text>
                 </Card.Body>
             </Card>
